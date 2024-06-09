64988989647b399f3178d1645aa9675c0e6cc666
achilezweb@gmail.com <achilezweb@gmail.com>
2024-06-09 14:48:35 +0000

    api boilerplate Task

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\TaskController;
 
 Route::get('/user', function (Request $request) {
@@ -10,6 +13,30 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/test', function (){
     return 'test';
+});
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
 });
 
 Route::apiResource('tasks', TaskController::class);
